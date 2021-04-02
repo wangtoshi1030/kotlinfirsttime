339eaba90b63987891ff82602bf0db4837135cc3
橋本昌林 <wangtoshiwork1881030@icloud.com>
2021-04-03 01:52:57 +0900

    todolistの画面のデザイン完成

--- app/src/main/java/com/example/practice/MainActivity.kt
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -9,10 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btn = findViewById<Button>(R.id.button);
-        btn.setOnClickListener {
-            Toast.makeText(this,"クリックしたよ",Toast.LENGTH_SHORT).show()
-        }
+
     }
 
 }
