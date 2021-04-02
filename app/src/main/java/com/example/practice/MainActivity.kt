58aba37b89300dca4b52a6cae72cc8157ea3c826
橋本昌林 <wangtoshiwork1881030@icloud.com>
2021-04-02 12:59:30 +0900

    Gitとgithubとsourcetreeをアンドロイドスタジオに連携完了

--- app/src/main/java/com/example/practice/MainActivity.kt
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.button);
+        btn.setOnClickListener {
+            Toast.makeText(this,"クリックしたよ",Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
